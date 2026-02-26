5fb3ec1c16375850f05850450a0c420f7cdc602f
Ian Rosevear <ian.rosevear@gmail.com>
2026-02-26 17:34:03 -0500

    new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,15 @@ const nunito = Nunito({
 
 export const metadata: Metadata = {
   title: "Ian's Christmas Village",
-  description: "A cozy corner of the internet",
+  description: "A collection of gifts from me to you.",
+  icons: {
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
