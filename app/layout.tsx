53921cf5a6e4f373271523412695aff95244fa92
ianrosevear <ian.rosevear@gmail.com>
2026-02-28 23:22:26 -0500

    organize public folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,12 +23,12 @@ export const metadata: Metadata = {
   description: "A collection of gifts from me to you.",
   icons: {
     icon: [
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/icons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/icons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
-    apple: "/apple-touch-icon.png",
+    apple: "/icons/apple-touch-icon.png",
   },
-  manifest: "/site.webmanifest",
+  manifest: "/icons/site.webmanifest",
 };
 
 export default function RootLayout({
