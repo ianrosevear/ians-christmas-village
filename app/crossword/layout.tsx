c6b662c009678ffd93e87296b5538fdf70f07207
ianrosevear <ian.rosevear@gmail.com>
2026-03-05 23:36:36 -0500

    update metadata to handle multiple crosswords

--- app/crossword/layout.tsx
+++ app/crossword/layout.tsx
@@ -3,11 +3,11 @@ import type { Metadata } from "next";
 // Because crossword page.tsx has "use client" it can't export metadata and needs a seperate layout to do that
 // Next.js automatically picks up any layout file in a route folder and wraps page.tsx with the layout
 export const metadata: Metadata = {
-  title: "Solid Start — Crossword",
-  description: "A crossword puzzle by Ian Rosevear.",
+  title: "Crosswords",
+  description: "Crossword puzzles by Ian Rosevear.",
   openGraph: {
-    title: "Solid Start — Crossword",
-    description: "A crossword puzzle by Ian Rosevear.",
+    title: "Crosswords",
+    description: "Crossword puzzles by Ian Rosevear.",
   },
 };
 
